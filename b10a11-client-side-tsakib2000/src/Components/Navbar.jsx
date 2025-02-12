93c03717becd12e36d8a62d1049e0d66a741ed8a
tsakib2000 <isakib49@gmail.com>
2025-02-12 11:19:15 +0600

    added dark mode

--- b10a11-client-side-tsakib2000/src/Components/Navbar.jsx
+++ b10a11-client-side-tsakib2000/src/Components/Navbar.jsx
@@ -1,9 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 import CarLogo from "../assets/icons8-car-rental-glyph-32.png";
 import useAuth from "../Hooks/useAuth";
+import { useEffect, useState } from "react";
 const Navbar = () => {
   const { user, signOutUser } = useAuth();
-
+  
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+   
+  };
+  useEffect(() => {
+    document.querySelector('html').setAttribute('data-theme', theme);
+  }, [theme]);
   const navbarLinks = (
     <>
       <li>
@@ -30,8 +40,8 @@ const Navbar = () => {
       )}
     </>
   );
-  return (
-    <div className="z-10 top-0 navbar bg-transparent backdrop-blur-2xl fixed  pt-4  pb-8 mx-auto">
+  return (<>
+    <div className="z-10 top-0 navbar bg-transparent backdrop-blur-2xl fixed  pt-4  pb-8 mx-auto flex flex-col md:flex-row">
 <div className="navbar w-11/12 mx-auto">
 <div className="navbar-start">
         <Link className="btn btn-ghost text-xl border border-gray-400">
@@ -42,7 +52,7 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex border border-gray-400 rounded-xl">
         <ul className="menu menu-horizontal px-1">{navbarLinks}</ul>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end    ">
         <div className="dropdown ">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
@@ -105,9 +115,51 @@ const Navbar = () => {
             </>
           )}
         </div>
+
+        
       </div>
+      
 </div>
+<div className=''>
+   <label className="flex cursor-pointer gap-2 ml-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <input
+              onClick={toggleTheme}
+                type="checkbox"
+                value="synthwave"
+                className="toggle theme-controller"
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
+   </div>
     </div>
+  
+    </>
   );
 };
 
