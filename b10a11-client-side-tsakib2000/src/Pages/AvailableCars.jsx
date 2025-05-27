a3c353bebe8e5c48d44a0f17cfbc991fc6915d73
tsakib2000 <isakib49@gmail.com>
2025-05-27 15:05:06 +0600

    fixed available car section responsiveness

--- b10a11-client-side-tsakib2000/src/Pages/AvailableCars.jsx
+++ b10a11-client-side-tsakib2000/src/Pages/AvailableCars.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 
-import { CiCircleList, CiGrid41 } from "react-icons/ci";
+import { CiCircleList, CiGrid41, CiSearch } from "react-icons/ci";
 import AvailableCarCard from "../Components/AvailableCarCard";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
 import LoadingSpinner from "../Components/LoadingSpinner";
 
 const AvailableCars = () => {
-  const axiosSecure=useAxiosSecure();
-  const [isGridView, setIsGridView] = useState(true);
-  const [search,setSearch]=useState('')
-  const [sort,setSort]=useState('')
+  const axiosSecure = useAxiosSecure();
+
+  const [search, setSearch] = useState('')
+  const [sort, setSort] = useState('')
   const [cars, setCars] = useState([]);
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
     fetchAvailableCars();
-  }, [search,sort]);
+  }, [search, sort]);
 
   const fetchAvailableCars = async () => {
     setLoading(true)
@@ -24,99 +24,79 @@ const AvailableCars = () => {
       setCars(data);
     } catch (err) {
       console.log(err.message);
-    }finally{
+    } finally {
       setLoading(false)
     }
   };
-const handleSortByPrice=(order)=>{
-  
- const newCars = [...cars].sort((a,b)=>{{
-    const priceA = parseInt(a.dailyRentalPrice) 
-    const priceB= parseInt(b.dailyRentalPrice)
-   return order === "asc" ? priceA - priceB : priceB - priceA;}});
- setCars(newCars)
+  const handleSortByPrice = (order) => {
+
+    const newCars = [...cars].sort((a, b) => {
+      {
+        const priceA = parseInt(a.dailyRentalPrice)
+        const priceB = parseInt(b.dailyRentalPrice)
+        return order === "asc" ? priceA - priceB : priceB - priceA;
+      }
+    });
+    setCars(newCars)
 
 
-}
+  }
 
   return (
-   <div>
- <h1 className="text-center my-4 text-2xl md:text-3xl font-bold">Available Rides</h1>
- <div className="flex flex-col md:flex-row justify-evenly items-center gap-5 ">
+    <div>
+      <h1 className="text-center my-4 text-2xl md:text-3xl font-bold">Available Rides</h1>
+      <div className=" grid grid-cols-12 gap-3">
 
-   <div>
-     <select
-        onChange={(e)=>handleSortByPrice(e.target.value)}
-       name="category"
-       id="category"
-       className="border p-4 rounded-lg"
-      
-     >
-       <option value="">Sort by Price</option>
-       <option  value="asc">Lowest First</option>
-       <option value="dsc"> Highest First</option>
-     </select>
-   </div>
+        <div className=" col-span-3 w-full">
+          <select
+            onChange={(e) => handleSortByPrice(e.target.value)}
+            name="category"
+            id="category"
+            className="border p-4 rounded-lg w-full"
 
-   <div>
-     <select
-       onChange={(e)=>setSort(e.target.value)}
-       name="category"
-       id="category"
-       className="border p-4 rounded-lg"
-    
-     >
-       <option value="">Sort by Date</option>
-       <option value="asc">Oldest First  </option>
-       <option value="dsc">Newest First </option>
-     </select>
-   </div>
+          >
+            <option value="">Sort by Price</option>
+            <option value="asc">Lowest First</option>
+            <option value="dsc"> Highest First</option>
+          </select>
+        </div>
 
-   <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
-     <input
-        onChange={e=>setSearch(e.target.value)}
-       className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
-       type="text"
-       name="search"
-       value={search}
-       placeholder="Enter Your Car Model"
-     />
+        <div className="col-span-3 w-full">
+          <select
+            onChange={(e) => setSort(e.target.value)}
+            name="category"
+            id="category"
+            className="border p-4 rounded-lg w-full"
 
-     <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
-       Search
-     </button>
-   </div>
+          >
+            <option value="">Sort by Date</option>
+            <option value="asc">Oldest First  </option>
+            <option value="dsc">Newest First </option>
+          </select>
+        </div>
 
-   {/* Grid and list Toggle button */}
+        <div className="col-span-6 flex justify-center items-center  p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+          <input
+            onChange={e => setSearch(e.target.value)}
+            className="px-6 py-2  text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+            type="text"
+            name="search"
+            value={search}
+            placeholder="Enter Your Car Model"
+          />
+          <CiSearch className="font-bold text-xl" />
+        </div>
 
-   <div className=" hidden md:inline bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex ">
-     <button
-       onClick={() => setIsGridView(true)}
-       className={`${
-         isGridView && "active"
-       } inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-l-full px-4 py-2" `}
-     >
-       <CiGrid41 />
-       <span>Grid</span>
-     </button>
-     <button
-       onClick={() => setIsGridView(false)}
-       className={`${isGridView || "active"} inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-r-full px-4 py-2`}
-     
-     >
-       <CiCircleList />
-       <span>List</span>
-     </button>
-   </div>
- </div>
+      </div>
 
-{
-  loading?<LoadingSpinner/>:<div className={`${isGridView && 'grid'} grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ${isGridView || 'w-3/5 lg:w-[518px] mx-auto'}`}>
-  {cars?.map(car=> <AvailableCarCard key={car._id} car={car}/>)}
-  </div>
-}
+      {
+        loading ? <LoadingSpinner /> : <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 'w-3/5  mx-auto'`}>
+          {cars?.map(car => <AvailableCarCard key={car._id} car={car} />)}
+        </div>
+      }
 
-</div>
+
+    </div>
   );
 };
 
